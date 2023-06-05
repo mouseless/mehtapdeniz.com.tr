f55e9410289c0262323c3c67e7764dc75d1968a2
Sefer Mirza <sefermirza@gmail.com>
2023-06-05 18:18:02 +0300

    remove stagging stage & add base url

--- .theme/nuxt.config.ts
+++ .theme/nuxt.config.ts
@@ -1,7 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: process.env.BASE_URL
+  },
   modules: ["@nuxt/content"],
   dir: {
     public: ".public"
+  },
+  content: {
+    markdown: {
+      remarkPlugins: {
+        "remark-emoji": false
+      }
+    }
+  },
+  router: {
+    options: {
+      strict: true
+    }
   }
 });
