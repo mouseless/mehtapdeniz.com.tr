0d0e6039f43e9cc70cec627f1a545a8f891c91bb
Sefer Mirza <sefermirza@gmail.com>
2023-06-05 21:20:29 +0300

    add assets from mouseless.brand

--- .theme/nuxt.config.ts
+++ .theme/nuxt.config.ts
@@ -1,7 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: process.env.BASE_URL
+    baseURL: process.env.BASE_URL,
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          type: "text/css",
+          href: "https://mouseless.github.io/brand/assets/css/primary.css"
+        }
+      ]
+    }
   },
   modules: ["@nuxt/content"],
   dir: {
