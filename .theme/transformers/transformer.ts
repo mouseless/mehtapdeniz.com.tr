ec80baf37292f64a15ef02cf2afa7ea988e67410
Sefer Mirza <36925434+SeferMirza@users.noreply.github.com>
2023-06-08 14:25:57 +0300

    add transformer

---
+++ .theme/transformers/transformer.ts
@@ -0,0 +1,14 @@
+// @ts-ignore
+import { defineTransformer } from "@nuxt/content/transformers";
+
+export default defineTransformer({
+  name: "learn-transformer",
+  extensions: [".md"],
+  transform (content: any) {
+    console.log(content);
+    if(content.title === "Transformers") {
+      // content.body.children.filter((f: any) => f.tag === "p")[0].tag = "strong";
+    }
+    return content;
+  }
+});
